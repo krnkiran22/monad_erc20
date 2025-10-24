513c4ee49a8be66eba9cea5e73e911ba97a1ba5e
krnkiran22 <kirandev2210@gmail.com>
2025-10-24 19:03:53 +0530

    tested

--- contracts/erc20.sol
+++ contracts/erc20.sol
@@ -1,21 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/security/Pausable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract CustomERC20 is ERC20, Pausable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    // Admin role is DEFAULT_ADMIN_ROLE, which can pause/unpause, grant/revoke roles (including transferring admin by granting to new address and renouncing), etc.
-
     constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(MINTER_ROLE, msg.sender);
-        // Initial supply can be minted here if desired, e.g., _mint(msg.sender, initialSupply);
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender); // Admin role for pausing, role management
+        _grantRole(MINTER_ROLE, msg.sender); // Minter role for minting tokens
     }
 
+    // Admin functions
     function pause() public onlyRole(DEFAULT_ADMIN_ROLE) {
         _pause();
     }
@@ -24,18 +22,18 @@ contract CustomERC20 is ERC20, Pausable, AccessControl {
         _unpause();
     }
 
+    // Minter function
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
-    // Hook to enforce pause on transfers and mints
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
+    // Override _update to integrate pausing functionality
+    function _update(address from, address to, uint256 amount)
         internal
         virtual
         override
     {
-        super._beforeTokenTransfer(from, to, amount);
-
         require(!paused(), "ERC20Pausable: token transfer while paused");
+        super._update(from, to, amount);
     }
 }
